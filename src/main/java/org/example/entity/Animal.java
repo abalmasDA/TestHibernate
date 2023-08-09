8c9c0cda6786d1cb422e63f3b89fb638f9217735
abalmasDA <abalmasit@gmail.com>
2023-08-09 22:13:51 +0300

    Commit 2

--- src/main/java/org/example/entity/Animal.java
+++ src/main/java/org/example/entity/Animal.java
@@ -23,35 +23,4 @@ public class Animal {
     public Animal() {
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isTail() {
-        return tail;
-    }
-
-    public void setTail(boolean tail) {
-        this.tail = tail;
-    }
 }
